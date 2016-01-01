9e1e5294f35f132185247362443b3ad4e2b569eb
Javi <fj.gutierrezs91@gmail.com>
2016-01-01 14:32:09 +0100

    Update Client.cpp

--- HelloICE/Client.cpp
+++ HelloICE/Client.cpp
@@ -15,10 +15,11 @@ main(int argc, char* argv[])
 	try {
 		ic = Ice::initialize(argc , argv);					    /* Same as Server */
 		Ice::ObjectPrx base = ic->stringToProxy("SimplePrinter:default -p 10000");  /* Obtain a proxy for the remote printer. We create a proxy by calling stringToProxy on the communicator, with the string "SimplePrinter:default -p 10000". Note that the string contains the object identity and the port number that were used by the server. */
-		PrinterPrx printer = PrinterPrx::checkedCast(base);
+		PrinterPrx printer = PrinterPrx::checkedCast(base);			    /* The proxy returned by stringToProxy is of type Ice::ObjectPrx but we need a proxy we use cast with calling PrinterPrx::checkedCast. */
+											    /* A checked cast sends a message to the server, effectively asking "is this a proxy for a Printer interface?" If so, the call returns a proxy to a Printer; otherwise, if the proxy denotes an interface of some other type, the call returns a null proxy.*/ 
 		if (!printer)
 			throw "Invalid Proxy";
-		printer->printString("Ice Hello world!! :)");
+		printer->printString("Ice Hello world!! :)");				    /* We make the calling to method printString to pass the string from client to server whose print in its Stdout the String */
 	
 	/* Exceptions of Client first for Ice communications and later for messages errors */
 	} catch (const Ice::Exception& ex){
